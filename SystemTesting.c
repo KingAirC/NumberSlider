3cebe0bbc51610d850b45b4d5a999b7c63f03f6b
KingAirC <40599926+KingAirC@users.noreply.github.com>
2018-07-28 13:13:37 +0800

    Initial commit

---
+++ SystemTesting.c
@@ -0,0 +1,32 @@
+#include "head.h"
+/*
+int main(){
+	
+	int i,j;
+	row=col=3;
+	
+	mat=calloc(col*row,sizeof(int));
+	for(i=0;i<row;i++){
+		*(mat+i)=calloc(col,sizeof(int));
+	}
+	
+
+	initMap();
+	
+	Map[0][1]=0;
+	Map[1][1]=0;
+	
+	mat[0][0]=2;mat[0][1]=1;mat[0][2]=3;
+	mat[1][0]=4;mat[1][1]=6;mat[1][2]=5;
+	mat[2][0]=7;mat[2][1]=8;mat[2][2]=0;
+	
+	//solve();
+	
+	POINT begin={0,0};
+	POINT end={0,2};
+	puts(findRoad(begin,end));
+	
+	
+return 0;
+}
+*/
